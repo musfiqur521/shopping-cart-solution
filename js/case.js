707cc58df2dd109c017326e89b576846c08f00af
musfiqur521 <musfiqur521@gmail.com>
2023-02-25 02:40:54 +0600

    Total-amount

--- js/case.js
+++ js/case.js
@@ -32,6 +32,7 @@ document.getElementById('btn-case-plus').addEventListener('click', function(){
     const newCaseNumber = updateCaseNumber(true)
 
     updateCaseTotalPrice(newCaseNumber);
+    calculateSubTotal();
 
 });
 
@@ -39,7 +40,8 @@ document.getElementById('btn-case-minus').addEventListener('click', function(){
 
     const newCaseNumber = updateCaseNumber(false)
 
-    updateCaseTotalPrice(newCaseNumber);
+    updateCaseTotalPrice(newCaseNumber);  
+    calculateSubTotal();
 })
 
 
--- js/phone.js
+++ js/phone.js
@@ -24,13 +24,23 @@ function updatePhoneTotalPrice(newPhoneNumber){
     const phoneTotalElement = document.getElementById('phone-total');
     phoneTotalElement.innerText = phoneTotalPrice;
 }
+
+
+
 document.getElementById('btn-phone-plus').addEventListener('click', function(){
 
     const newPhoneNumber = updatePhoneNumber(true);
+
     updatePhoneTotalPrice(newPhoneNumber);
-})
+    calculateSubTotal();
+    //calculate total
+
+
+});
 
 document.getElementById('btn-phone-minus').addEventListener('click', function(){
 
     const newPhoneNumber = updatePhoneNumber(false);
+    updatePhoneTotalPrice(newPhoneNumber);
+    calculateSubTotal();
 })
